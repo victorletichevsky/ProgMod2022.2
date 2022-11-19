fd64d5fd2b165eba44dec214f5ebed3f4741567c
Paulo Fontes <78375893+pbfontes@users.noreply.github.com>
2022-11-19 17:41:55 -0300

    ajustes de documentação - grupo 3

--- App/app.py
+++ App/app.py
@@ -10,6 +10,9 @@ temas_habilitados = ("todos", "Trabalho", "Meta", "Familia", "Faculdade")
 
 @app.route("/", methods=['GET'])
 def home():
+	"""
+	Show messages based on the filter chosen by the user. If this filter doesn't exist, it shows all messages.
+	"""
 	tema = request.args.get("tema")
 	if tema in temas_habilitados:
 		# se um tema correto foi passado, o usaremos para filtrar
@@ -22,6 +25,9 @@ def home():
 
 @app.route('/filtro', methods=['POST'])
 def filter():
+	"""
+	Uses the filter chosen by the user to redirect him to the page of this filter.
+	"""
 	if request.method == 'POST':
 		tema = request.form.get("tema")
 		return redirect(f"/?tema={tema}")
@@ -29,6 +35,9 @@ def filter():
 
 @app.route('/msg', methods=["POST"])
 def upload():
+	"""
+	Verify if message, user and theme are valids. If they are, store them on Firebase.
+	"""
 	if request.method == 'POST':
 		tema = request.form.get("tema")
 		msg = request.form.get("msg")
@@ -49,5 +58,5 @@ def upload():
 	return redirect("/")
 
 if __name__ == '__main__':
-	#app.debug = True
-	app.run()
+	app.debug = True
+	#app.run()
