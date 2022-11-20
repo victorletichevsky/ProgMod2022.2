1b0ac1ce6c82d9fcad9e27e64f5f37ccafa6366b
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-11-19 22:00:07 -0300

    das,opd

--- Grupo_2/login-app/test/login_test.dart
+++ Grupo_2/login-app/test/login_test.dart
@@ -1,118 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:login_app/constants.dart';
 import 'package:login_app/main.dart';
-import 'package:login_app/page/profile.dart';
 
 void main() {
   testWidgets('MyApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const Myapp());
+    await tester.pumpWidget(Myapp());
     // Add tests here!
     void test() {}
 
     Widget build(BuildContext context) {
-      final myCPF = TextEditingController();
-      final myPW = TextEditingController();
       return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 80.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Login app",
+          appBar: AppBar(
+            title: const Text("Profile Page"),
+            backgroundColor: Colors.black12,
+            elevation: 2.0,
+          ),
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                const Center(
+                  child: Text(
+                    "Gustavo Soares",
                     style: TextStyle(
-                        color: Colors.black,
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 28),
                   ),
-                  Text(
-                    "Faça login para acessar suas informações",
-                    style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+                const Center(
+                  child: Text(
+                    "20, Rio de Janeiro",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.1,
-                  ),
-                  TextField(
-                    controller: myCPF,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      hintText: "CPF",
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  TextField(
-                    controller: myPW,
-                    obscureText: true,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      hintText: "Senha",
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.02,
-                  ),
-                  Text(
-                    "Não lembra da sua senha?",
-                    style: TextStyle(color: Colors.blue, fontSize: 14),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.3,
-                        height: MediaQuery.of(context).size.height * 0.07,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (myPW.text == "123456" &&
-                                myCPF.text == "123456") {
-                              Navigator.pushNamed(context, Profile.id);
-                            }
-                          },
-                          child: Text("Login"),
-                          style: ElevatedButton.styleFrom(
-                            primary: kBackgroundColor2,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(1),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ),
-      );
+          ));
     }
   });
 }
--- Grupo_2/login-app/test/profile_test.dart
+++ Grupo_2/login-app/test/profile_test.dart
@@ -1,47 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:login_app/main.dart';
-import 'package:login_app/widgets/textbox.dart';
-import 'package:login_app/widgets/photopicker.dart';
 
 void main() {
   testWidgets('MyApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const Myapp());
+    await tester.pumpWidget(Myapp());
     // Add tests here!
     void test() {}
 
     Widget build(BuildContext context) {
       return Scaffold(
-          appBar: AppBar(
-            title: const Text("Profile Page"),
-            backgroundColor: Colors.black12,
-            elevation: 2.0,
-          ),
-          body: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                PhotoPicker(context, test, "Adicione uma foto"),
-                const Center(
-                  child: Text(
-                    "Gustavo Soares",
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Login app",
                     style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
                         fontWeight: FontWeight.bold,
                         fontSize: 28),
                   ),
-                ),
-                const Center(
-                  child: Text(
-                    "20, Rio de Janeiro",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  Text(
+                    "Faça login para acessar suas informações",
+                    style: TextStyle(color: Colors.black, fontSize: 16),
                   ),
-                ),
-                TextBox(context, "Conte um pouco sobre você"),
-              ],
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      hintText: "CPF",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      hintText: "Senha",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Text(
+                    "Não lembra da sua senha?",
+                    style: TextStyle(color: Colors.blue, fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Login"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(1),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ));
+          ),
+        ),
+      );
     }
   });
 }
