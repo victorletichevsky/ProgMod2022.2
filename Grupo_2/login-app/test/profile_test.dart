511912bcada4aafbb0a048aea1bf9201cff8a7c2
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-11-19 22:08:30 -0300

    asdasd

--- Grupo_2/login-app/test/profile_test.dart
+++ Grupo_2/login-app/test/profile_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:login_app/main.dart';
+import 'package:Grupo_2/login_app/main.dart';
 
 void main() {
   testWidgets('MyApp smoke test', (WidgetTester tester) async {
