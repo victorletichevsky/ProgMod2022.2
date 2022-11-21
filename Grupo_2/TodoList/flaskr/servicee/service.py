ed6a0b3dfb9c3d508285533f990d04b85237692e
JP Fernandes <joaopedro.jpbf@gmail.com>
2022-11-20 21:25:31 -0300

    Atualização do service.py

--- Grupo_2/TodoList/flaskr/servicee/service.py
+++ Grupo_2/TodoList/flaskr/servicee/service.py
@@ -7,37 +7,79 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 messageRef = db.collection('afazeres')
 
-def createMessage(user, message, theme):
+def createMessage(user, message, theme, docRef = messageRef):
+    '''
+    Creates a new message and send it to firebase.
+    
+    Parameters
+    ----------
+        user : str
+            Is the person who writes the message
+        message : str
+            Is the text in the message
+        theme : str
+            Is the topic of the message
+        docRef : Any, optional
+           the document reference
+    '''
     try:
-        messageRef.add({
+        docRef.add({
             "user": user,
             "message": message,
             "theme": theme
         })
-        return
+        return 1
     except:
         print("Erro criando item")
-        return 
+        return 0
 
-def getMessages(filter = "Todos"):
-	messageList = list()
-	try:
-		allMessages = messageRef.get()
-		if (filter == "Todos"):
-			for currentMessage in allMessages:
-				messageList.append(currentMessage.to_dict())
-			return messageList
-		else:
-			messageList = messageFilter(filter)
-			return messageList
-	except:
-		print("Erro ao baixar itens")
-		return
+def getMessages(filter = "Todos", docRef = messageRef):
+    ''' 
+    Gets the messages from firebase and return a list of messages.
+    
+    Parameters
+    ----------
+    filter : str, optional
+           theme of the messages (default is "Todos")
+    docRef : Any, optional
+           the document reference
+    Returns
+    -------
+    list
+        A list of messages
+    '''
+    messageList = list()
+    try:
+        allMessages = docRef.get()
+        if (filter == "Todos"):
+            for currentMessage in allMessages:
+                messageList.append(currentMessage.to_dict())
+            return messageList
+        else:
+            messageList = messageFilter(filter)
+        return messageList
+    except:
+        print("Erro ao baixar itens")
+        return
 
-def messageFilter(filter):
+def messageFilter(filter, docRef = messageRef):
+    '''
+    Gets the messages from firebase, filter them and return a list of filtered messages.
+    
+    Parameters
+    ----------
+    filter : str
+           theme of the messages
+    docRef : Any, optional
+           the document reference
+    Returns
+    -------
+    list
+        A list of filtered messages
+    '''
     filteredMessageList = list()
     try:
-        allMessages = messageRef.get()
+        allMessages = docRef.get()
         for currentMessage in allMessages:
             if (currentMessage.get("theme") == filter):
                 filteredMessageList.append(currentMessage.to_dict())
@@ -45,6 +87,3 @@ def messageFilter(filter):
     except:
         print("Error filtrando itens")
         return 
-        
-def helloworld():
-    return 'hello world'
